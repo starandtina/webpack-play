e420eac72299f0f8a1bd1ceece3367f8b020afd8
starandtina <kandmforever@126.com>
2016-01-02 15:10:31 +0800

    first commit

---
+++ entry.js
@@ -0,0 +1,5 @@
+let foo = require('./foo');
+let bar = require('./bar');
+let myVariable = `${q} bottles of beer on the wall, ${q} bottles of beer.`;
+
+module.exports = myVariable;
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel'
+    }
+  ]
+  }
+};
